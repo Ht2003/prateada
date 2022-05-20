ae40d645d7a35931519c89ddbecb5a33bcebf1c1
Ht2003 <105807771+Ht2003@users.noreply.github.com>
2022-05-20 01:56:04 -0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,13 @@
+lista=[] # duas listas para comparar 
+L2=[]
+N=int(input("N="))#N valores
+for i in range(N):
+    NI=int(input ("digite um número inteiro positivo"))
+    lista.append(NI)
+    L2.append(NI)#adicionar NI nas listas 
+    L2.sort()#essa função faz com que a lista 2 sempre saia em ordem crescente 
+if lista == L2: #comparação das listas
+  print("está em ordem crescente" )
+elif lista != L2:
+  print(" não está em ordem crescente")
+  
