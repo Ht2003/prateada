8fb2c699707669990af1daa416822e091d1fa3db
Ht2003 <105807771+Ht2003@users.noreply.github.com>
2022-05-21 10:11:33 -0300

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,18 @@
+#o usuário deve informar os três valores
+x = int(input ("Informe o valor de x: "))
+y = int(input("Informe o valor de y: "))
+z = int(input("Informe o valor de z: "))
+#formam os intervalos dos valores inteiros que o usuário digitar 
+while True:
+       if  x  <  y:
+          if   z  <=  y: 
+                   break
+          else:
+                  z = int(input("Informe o valor de z: "))
+       else: 
+              x = int(input ("Informe o valor de x: "))
+# os intervalos de x e y divisíveis por z
+print("\nValores inteiros entre x e y divisiveis por z:")
+[print (i) for i in range(x, y + 1) if i % z == 0]
+print("\nSoma dos valores pares impressos") 
+print(sum([i for i in range(x, y + 1) if i % z == 0 and i % 2 == 0]))
